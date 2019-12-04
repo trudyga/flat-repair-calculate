a5511c02d8851924265525a700c4eef47916c43a
trudyga <trudyga0822@gmail.com>
2019-12-04 04:46:32 +0200

    Updates

---
+++ precache-manifest.880f43784c5dd8b3a7117337e89b4127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "580f120e130533ae980fe5021047a91c",
+    "url": "/flat-repair-calculate/index.html"
+  },
+  {
+    "revision": "31ba81c629afeb95caf0",
+    "url": "/flat-repair-calculate/static/css/main.2682208a.chunk.css"
+  },
+  {
+    "revision": "7d9efbf46d9b791646e7",
+    "url": "/flat-repair-calculate/static/js/2.5b3c281f.chunk.js"
+  },
+  {
+    "revision": "31ba81c629afeb95caf0",
+    "url": "/flat-repair-calculate/static/js/main.d2900465.chunk.js"
+  },
+  {
+    "revision": "e2e59164726e2757a393",
+    "url": "/flat-repair-calculate/static/js/runtime-main.b9dea54b.js"
+  },
+  {
+    "revision": "45a382d7a1c2edb34b5bf68343231985",
+    "url": "/flat-repair-calculate/static/media/minusIcon.45a382d7.svg"
+  },
+  {
+    "revision": "696a97cb0b705a595d855d4572fd3fa9",
+    "url": "/flat-repair-calculate/static/media/plusIcon.696a97cb.svg"
+  }
+]);
